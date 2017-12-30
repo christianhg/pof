fb7f12e222ae3ce90db4871f033531ecb99a0b61
Christian Hamburger Grøngaard <christianhg@gmail.com>
2017-12-30 14:30:27 +0100

    Flip f and g in compose

--- src/core/compose.ts
+++ src/core/compose.ts
@@ -1,3 +1,3 @@
-export function compose<A, B, C>(f: (b: B) => C, g: (a: A) => B) {
-  return (a: A) => f(g(a))
+export function compose<A, B, C>(g: (b: B) => C, f: (a: A) => B) {
+  return (a: A) => g(f(a))
 }
